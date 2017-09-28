3c86b64b655307087e603390f0604e98e2d98b94
NicolasDorier <nicolas.dorier@gmail.com>
2017-09-28 10:49:40 +0900

    Add info to git tag, bump

--- CommandLine.Configuration/PushNuget.ps1
+++ CommandLine.Configuration/PushNuget.ps1
@@ -2,5 +2,5 @@ rm "bin\release\" -Recurse -Force
 dotnet pack --configuration Release
 dotnet nuget push "bin\Release\" --source "https://api.nuget.org/v3/index.json"
 $ver = ((ls .\bin\release\*.nupkg)[0].Name -replace '[^\d]*\.(\d+(\.\d+){1,4}).*', '$1')
-git tag -a "v$ver" -m "$ver"
+git tag -a "Configuration/v$ver" -m "$ver"
 git push --tags
