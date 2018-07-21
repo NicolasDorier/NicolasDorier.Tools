baa4056383ccbe6b59075a0892bcdc372f2c87c9
nicolas.dorier <nicolas.dorier@gmail.com>
2018-07-21 22:53:50 +0900

    Does not attempt to create directory if createIfNotExists is false

--- StandardConfiguration/DefaultDataDirectory.cs
+++ StandardConfiguration/DefaultDataDirectory.cs
@@ -7,38 +7,42 @@ namespace StandardConfiguration
 {
     public class DefaultDataDirectory
     {
-		public static string GetDirectory(string appDirectory, string subDirectory, bool createIfNotExists = true)
-		{
-			string directory = null;
-			var home = Environment.GetEnvironmentVariable("HOME");
-			if(!string.IsNullOrEmpty(home))
-			{
-				directory = home;
-				directory = Path.Combine(directory, "." + appDirectory.ToLowerInvariant());
-			}
-			else
-			{
-				var localAppData = Environment.GetEnvironmentVariable("APPDATA");
-				if(!string.IsNullOrEmpty(localAppData))
-				{
-					directory = localAppData;
-					directory = Path.Combine(directory, appDirectory);
-				}
-				else
-				{
-					throw new DirectoryNotFoundException("Could not find suitable datadir");
-				}
-			}
-			if(!Directory.Exists(directory))
-			{
-				Directory.CreateDirectory(directory);
-			}
-			directory = Path.Combine(directory, subDirectory);
-			if(createIfNotExists && !Directory.Exists(directory))
-			{
-				Directory.CreateDirectory(directory);
-			}
-			return directory;
-		}
-	}
+        public static string GetDirectory(string appDirectory, string subDirectory, bool createIfNotExists = true)
+        {
+            string directory = null;
+            var home = Environment.GetEnvironmentVariable("HOME");
+            if(!string.IsNullOrEmpty(home))
+            {
+                directory = home;
+                directory = Path.Combine(directory, "." + appDirectory.ToLowerInvariant());
+            }
+            else
+            {
+                var localAppData = Environment.GetEnvironmentVariable("APPDATA");
+                if(!string.IsNullOrEmpty(localAppData))
+                {
+                    directory = localAppData;
+                    directory = Path.Combine(directory, appDirectory);
+                }
+                else
+                {
+                    throw new DirectoryNotFoundException("Could not find suitable datadir");
+                }
+            }
+
+            if(createIfNotExists)
+            {
+                if(!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+                directory = Path.Combine(directory, subDirectory);
+                if(!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+            }
+            return directory;
+        }
+    }
 }
